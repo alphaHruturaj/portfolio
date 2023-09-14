c2df94ffbe02df7c768ea642f947ca750862197e
alphaHruturaj <contacthruturaj16@gmail.com>
2023-09-14 22:27:51 +0530

    my third commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-          Hruturaj's Portfolio
+          Hruturaj's Portfolio website
         </h1>
       </header>
     </div>
